de5e0e91a8598cfab853353bef687640849d3334
exzette <Erik.Zettersten@elavon.com>
2020-03-15 23:18:31 -0400

    docker updates

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const fastify = require('fastify')();
-const mjml = require('mjml');
+const fastify = require("fastify")();
+const mjml = require("mjml");
 
-fastify.post('/', function (request, reply) {
-    if (typeof request.body.mjml === 'undefined' || request.body.mjml === null) {
-        reply.send({});
-        return;
-    }
+fastify.post("/", function(request, reply) {
+    
+  if (typeof request.body.mjml === "undefined" || request.body.mjml === null) {
+    reply.send({});
+    return;
+  }
 
-    let result = mjml(request.body.mjml);
+  let result = mjml(request.body.mjml);
 
-    if (Object.keys(result.errors).length) {
-        Object.keys(result.errors).forEach((key) => {
-            delete result.errors[key].formattedMessage;
-        });
-    }
+  if (Object.keys(result.errors).length) {
+    Object.keys(result.errors).forEach(key => {
+      delete result.errors[key].formattedMessage;
+    });
+  }
 
-    reply.send(result)
+  reply.send(result);
 });
 
 // Run the server!
-fastify.listen(3000, '127.0.0.1', function (err) {
+fastify.listen(
+  process.env.PORT || 3000,
+  process.env.HOST || "127.0.0.1",
+  function(err) {
     if (err) throw err;
-    console.log(`server listening on ${fastify.server.address().port}`)
-});
+    console.log(`server listening on ${fastify.server.address().port}`);
+  }
+);
